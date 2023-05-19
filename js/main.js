1928b8557f498d20c54051dfb021e2a4f903d28c
1x55 <k4i@pm.me>
2023-05-19 17:17:01 +0900

    added class constructor

--- js/main.js
+++ js/main.js
@@ -3,14 +3,26 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        const potentialPet = new Poke ())_
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+
+  class Poke {
+    constructor (name, height, weight, types, image) {
+        this.name = name
+        this.height = height
+        this.types = types
+        this.image = image
+        this.weight = weight
+        this.housepet = true
+        this.reason = []
+  }
 }

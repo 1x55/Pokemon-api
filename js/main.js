832174a6bde39ada6647f7a4c75252a5b59a76ed
1x55 <k4i@pm.me>
2023-05-21 15:48:12 +0900

    updated class constructor

--- js/main.js
+++ js/main.js
@@ -11,7 +11,16 @@ function getFetch(){
         console.log(data)
         const potentialPet = new Poke (data.name, data.height, data.weight, data.types, data.sprites.other['official-artwork'].front_default)
         potentialPet.getTypes();
-        })
+        potentialPet.isItHousepet(); //important
+        let decision = ''
+        if (potentialPet.housepet) {
+            decision = 'This Pokemon is small enough, light enough, and safe enough to be a good pet!'
+        } else {
+            decision = `This Pokemon would not be a good pet because: ${potentialPet.reason.join(' and ')}.`
+        }
+        document.querySelector('h2').innerText = decision
+        document.querySelector('img').src = potentialPet.image
+     })
       .catch(err => {
           console.log(`error ${err}`)
       });
@@ -44,5 +53,18 @@ function getFetch(){
   }
   isItHousepet() {
     //check height, weight and types
+    let badTypes = ['fire', 'electric', 'fighting', 'poison', 'ghost', 'psychic'];
+    if (this.weightToPounds(this.weight) > 400) {
+        this.reason.push(`it is too heavy at ${this.weight} pounds`)
+        this.housepet = false
+    }
+    if (this.heightToFeet(this.height) > 7) {
+        this.reason.push(`it is too tall at ${this.height} feet`)
+        this.housepet = false
+    }
+    if (badTypes.some (r => this.typeList.indexOf(r) >= 0)) {
+        this.reason.push(`it has a bad type of ${this.typeList.join(', ')}`)
+        this.housepet = false
+    }
   }
 }

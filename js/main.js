eed8b20667babc5041a827edc20de9b2ef8dcdb8
1x55 <k4i@pm.me>
2023-06-01 14:08:40 +0900

    added functions

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        const potentialPet = new Poke ())_
-      })
+        const potentialPet = new Poke (data.name, data.height, data.weight, data.types, data.sprites.other['official-artwork'].front_default)
+        potentialPet.getTypes();
+        })
       .catch(err => {
           console.log(`error ${err}`)
       });
-
+    }
   class Poke {
     constructor (name, height, weight, types, image) {
         this.name = name
@@ -24,5 +25,24 @@ function getFetch(){
         this.weight = weight
         this.housepet = true
         this.reason = []
+        this.typeList = []
+  }
+
+  getTypes() {
+    for (const property of this.types) {
+        this.typeList.push(property.type.name)
+    }
+    console.log(this.typeList)
+  }
+
+  weightToPounds(weight) {
+    return Math.round((weight/4.536)*100)/100
+  }
+
+  heightToFeet (height) {
+    return Math.round((height/3.048)*100)/100
+  }
+  isItHousepet() {
+    //check height, weight and types
   }
 }
